d881b63b0a32414ba3a8f79187c0646fd38a146e
Bangqi Zhu <bangqizhu@microsoft.com>
2025-01-15 02:27:03 -0800

    RAGEngine e2e tests
    Signed-off-by: Bangqi Zhu <bangqizhu@microsoft.com>

---
+++ test/rage2e/e2e_test.go
@@ -0,0 +1,130 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+
+	"github.com/kaito-project/kaito/test/rage2e/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	ctx                 = context.Background()
+	namespaceName       = fmt.Sprint(utils.E2eNamespace, rand.Intn(100))
+	nodeProvisionerName = os.Getenv("TEST_SUITE")
+)
+
+var _ = BeforeSuite(func() {
+	utils.GetClusterClient(utils.TestingCluster)
+
+	namespaceName = fmt.Sprintf("%s-%d", namespaceName, GinkgoParallelProcess())
+	GinkgoWriter.Printf("Namespace %q for e2e tests\n", namespaceName)
+
+	kaitoNamespace := os.Getenv("KAITO_NAMESPACE")
+
+	if nodeProvisionerName == "azkarpenter" {
+		karpenterNamespace := os.Getenv("KARPENTER_NAMESPACE")
+		//check karpenter deployment is up and running
+		karpenterDeployment := &v1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "karpenter",
+				Namespace: karpenterNamespace,
+			},
+		}
+
+		Eventually(func() error {
+			return utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: karpenterDeployment.Namespace,
+				Name:      karpenterDeployment.Name,
+			}, karpenterDeployment, &client.GetOptions{})
+		}, utils.PollTimeout, utils.PollInterval).
+			Should(Succeed(), "Failed to wait for	karpenter deployment")
+	}
+
+	if nodeProvisionerName == "gpuprovisioner" {
+		gpuNamespace := os.Getenv("GPU_PROVISIONER_NAMESPACE")
+		//check gpu-provisioner deployment is up and running
+		gpuProvisionerDeployment := &v1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "gpu-provisioner",
+				Namespace: gpuNamespace,
+			},
+		}
+
+		Eventually(func() error {
+			return utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: gpuProvisionerDeployment.Namespace,
+				Name:      gpuProvisionerDeployment.Name,
+			}, gpuProvisionerDeployment, &client.GetOptions{})
+		}, utils.PollTimeout, utils.PollInterval).
+			Should(Succeed(), "Failed to wait for	gpu-provisioner deployment")
+	}
+
+	//check kaito-workspace deployment is up and running
+
+	kaitoWorkspaceDeployment := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "kaito-workspace",
+			Namespace: kaitoNamespace,
+		},
+	}
+
+	bqzkaitoWorkspaceDeploymentList := &v1.DeploymentList{}
+
+	Eventually(func() error {
+		// for listing all the deployment
+		return utils.TestingCluster.KubeClient.List(ctx, bqzkaitoWorkspaceDeploymentList, &client.ListOptions{})
+	}, utils.PollTimeout, utils.PollInterval).Should(Succeed(), "Failed to list for all deployments")
+	GinkgoWriter.Printf("bqzkaitoWorkspaceDeploymentList: %v\n", bqzkaitoWorkspaceDeploymentList)
+
+	Eventually(func() error {
+		return utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+			Namespace: kaitoWorkspaceDeployment.Namespace,
+			Name:      kaitoWorkspaceDeployment.Name,
+		}, kaitoWorkspaceDeployment, &client.GetOptions{})
+	}, utils.PollTimeout, utils.PollInterval).Should(Succeed(), "Failed to wait for	kaito-workspace deployment")
+
+	// create testing namespace
+	err := utils.TestingCluster.KubeClient.Create(context.TODO(), &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: namespaceName,
+		},
+	})
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	// delete testing namespace
+	Eventually(func() error {
+		return utils.TestingCluster.KubeClient.Delete(ctx, &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: namespaceName,
+			},
+		}, &client.DeleteOptions{})
+	}, utils.PollTimeout, utils.PollInterval).Should(Succeed(), "Failed to delete namespace for e2e")
+
+})
+
+func RunE2ETests(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "AI Toolchain Operator E2E Test Suite")
+}
+
+func TestE2E(t *testing.T) {
+	RunE2ETests(t)
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
---
+++ test/rage2e/rag_test.go
@@ -0,0 +1,480 @@
+package e2e
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/samber/lo"
+	appsv1 "k8s.io/api/apps/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	kaitov1alpha1 "github.com/kaito-project/kaito/api/v1alpha1"
+	"github.com/kaito-project/kaito/test/rage2e/utils"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	PresetPhi3Mini128kModel = "phi-3-mini-128k-instruct"
+)
+
+func loadTestEnvVars() {
+	var err error
+	runLlama13B, err = strconv.ParseBool(os.Getenv("RUN_LLAMA_13B"))
+	if err != nil {
+		fmt.Print("Error: RUN_LLAMA_13B ENV Variable not set")
+		runLlama13B = false
+	}
+
+	// Required for Llama models
+	aiModelsRegistry = utils.GetEnv("AI_MODELS_REGISTRY")
+	aiModelsRegistrySecret = utils.GetEnv("AI_MODELS_REGISTRY_SECRET")
+	// Currently required for uploading fine-tuning results
+	e2eACRSecret = utils.GetEnv("E2E_ACR_REGISTRY_SECRET")
+	supportedModelsYamlPath = utils.GetEnv("SUPPORTED_MODELS_YAML_PATH")
+	azureClusterName = utils.GetEnv("AZURE_CLUSTER_NAME")
+}
+
+func loadModelVersions() {
+	// Load stable model versions
+	configs, err := utils.GetModelConfigInfo(supportedModelsYamlPath)
+	if err != nil {
+		fmt.Printf("Failed to load model configs: %v\n", err)
+		os.Exit(1)
+	}
+
+	modelInfo, err = utils.ExtractModelVersion(configs)
+	if err != nil {
+		fmt.Printf("Failed to extract stable model versions: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+var runLlama13B bool
+var aiModelsRegistry string
+var aiModelsRegistrySecret string
+var e2eACRSecret string
+var supportedModelsYamlPath string
+var modelInfo map[string]string
+var azureClusterName string
+
+var _ = Describe("RAGEngine preset", func() {
+	BeforeEach(func() {
+		loadTestEnvVars()
+		loadModelVersions()
+	})
+
+	AfterEach(func() {
+		if CurrentSpecReport().Failed() {
+			utils.PrintPodLogsOnFailure(namespaceName, "")     // The Preset Pod
+			utils.PrintPodLogsOnFailure("kaito-ragengine", "") // The Kaito Workspace Pod
+			utils.PrintPodLogsOnFailure("gpu-provisioner", "") // The gpu-provisioner Pod
+			Fail("Fail threshold reached")
+		}
+	})
+
+	It("should create a Phi-3-mini-128k-instruct workspace with preset public mode successfully", func() {
+		numOfNode := 1
+		workspaceObj := createPhi3WorkspaceWithPresetPublicModeAndVLLM(numOfNode)
+
+		time.Sleep(30 * time.Second)
+
+		validateCreateNode(workspaceObj, numOfNode)
+		validateWorkspaceResourceStatus(workspaceObj)
+
+		time.Sleep(30 * time.Second)
+
+		validateAssociatedService(workspaceObj.ObjectMeta)
+		validateInferenceConfig(workspaceObj)
+
+		validateInferenceandRAGResource(workspaceObj.ObjectMeta, int32(numOfNode), false)
+
+		validateWorkspaceReadiness(workspaceObj)
+
+		serviceName := workspaceObj.Name
+		serviceNamespace := workspaceObj.Namespace
+		service := &v1.Service{}
+
+		_ = utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+			Namespace: serviceNamespace,
+			Name:      serviceName,
+		}, service)
+
+		clusterIP := service.Spec.ClusterIP
+
+		GinkgoWriter.Printf("ClusterIP: %s", clusterIP)
+
+		ragengineObj := createRAGEngine(clusterIP)
+
+		defer cleanupResources(workspaceObj, ragengineObj)
+
+		validateRAGEngineResourceStatus(ragengineObj)
+		validateAssociatedService(ragengineObj.ObjectMeta)
+		validateInferenceandRAGResource(ragengineObj.ObjectMeta, int32(numOfNode), false)
+		validateRAGEngineReadiness(ragengineObj)
+
+	})
+})
+
+func createPhi3WorkspaceWithPresetPublicModeAndVLLM(numOfNode int) *kaitov1alpha1.Workspace {
+	workspaceObj := &kaitov1alpha1.Workspace{}
+	By("Creating a workspace CR with Phi-3-mini-128k-instruct preset public mode and vLLM", func() {
+		uniqueID := fmt.Sprint("preset-phi3-", rand.Intn(1000))
+		workspaceObj = utils.GenerateInferenceWorkspaceManifestWithVLLM(uniqueID, namespaceName, "", numOfNode, "Standard_NC6s_v3",
+			&metav1.LabelSelector{
+				MatchLabels: map[string]string{"kaito-workspace": "public-preset-e2e-test-phi-3-mini-128k-instruct-vllm"},
+			}, nil, PresetPhi3Mini128kModel, kaitov1alpha1.ModelImageAccessModePublic, nil, nil, nil)
+
+		createAndValidateWorkspace(workspaceObj)
+	})
+	return workspaceObj
+}
+
+func createAndValidateWorkspace(workspaceObj *kaitov1alpha1.Workspace) {
+	By("Creating workspace", func() {
+		Eventually(func() error {
+			return utils.TestingCluster.KubeClient.Create(ctx, workspaceObj, &client.CreateOptions{})
+		}, utils.PollTimeout, utils.PollInterval).
+			Should(Succeed(), "Failed to create workspace %s", workspaceObj.Name)
+
+		By("Validating workspace creation", func() {
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: workspaceObj.Namespace,
+				Name:      workspaceObj.Name,
+			}, workspaceObj, &client.GetOptions{})
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+}
+func createAndValidateRAGEngine(ragEngineObj *kaitov1alpha1.RAGEngine) {
+	By("Creating ragEngine", func() {
+		Eventually(func() error {
+			return utils.TestingCluster.KubeClient.Create(ctx, ragEngineObj, &client.CreateOptions{})
+		}, utils.PollTimeout, utils.PollInterval).
+			Should(Succeed(), "Failed to create ragEngine   %s", ragEngineObj.Name)
+
+		By("Validating ragEngine creation", func() {
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: ragEngineObj.Namespace,
+				Name:      ragEngineObj.Name,
+			}, ragEngineObj, &client.GetOptions{})
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+}
+
+func createRAGEngine(baseURL string) *kaitov1alpha1.RAGEngine {
+	ragEngineObj := &kaitov1alpha1.RAGEngine{}
+	serviceURL := fmt.Sprintf("%s/v1/completions", baseURL)
+	By("Creating a workspace with adapter", func() {
+		uniqueID := fmt.Sprint("rag-", rand.Intn(1000))
+		ragEngineObj = GenerateRAGEngineManifest(uniqueID, namespaceName, "Standard_NC6s_v3", "BAAI/bge-small-en-v1.5",
+			&metav1.LabelSelector{
+				MatchLabels: map[string]string{"apps": "phi-3"},
+			},
+			&kaitov1alpha1.InferenceServiceSpec{
+				URL: serviceURL,
+			},
+		)
+
+		createAndValidateRAGEngine(ragEngineObj)
+	})
+	return ragEngineObj
+}
+
+func GenerateRAGEngineManifest(name, namespace, instanceType, embeddingModelID string, labelSelector *metav1.LabelSelector, inferenceURL *kaitov1alpha1.InferenceServiceSpec) *kaitov1alpha1.RAGEngine {
+	return &kaitov1alpha1.RAGEngine{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: &kaitov1alpha1.RAGEngineSpec{
+			Compute: &kaitov1alpha1.ResourceSpec{
+				InstanceType:  instanceType,
+				LabelSelector: labelSelector,
+			},
+			Embedding: &kaitov1alpha1.EmbeddingSpec{
+				Local: &kaitov1alpha1.LocalEmbeddingSpec{
+					ModelID: embeddingModelID,
+				},
+			},
+			InferenceService: inferenceURL,
+		},
+	}
+}
+
+// validateWorkspaceReadiness validates workspace readiness
+func validateWorkspaceReadiness(workspaceObj *kaitov1alpha1.Workspace) {
+	By("Checking the workspace status is ready", func() {
+		Eventually(func() bool {
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: workspaceObj.Namespace,
+				Name:      workspaceObj.Name,
+			}, workspaceObj, &client.GetOptions{})
+
+			if err != nil {
+				return false
+			}
+
+			_, conditionFound := lo.Find(workspaceObj.Status.Conditions, func(condition metav1.Condition) bool {
+				return condition.Type == string(kaitov1alpha1.WorkspaceConditionTypeSucceeded) &&
+					condition.Status == metav1.ConditionTrue
+			})
+			return conditionFound
+		}, 10*time.Minute, utils.PollInterval).Should(BeTrue(), "Failed to wait for workspace to be ready")
+	})
+}
+
+// validateRAGEngineReadiness validates ragengine readiness
+func validateRAGEngineReadiness(ragengineObj *kaitov1alpha1.RAGEngine) {
+	By("Checking the ragengine status is ready", func() {
+		Eventually(func() bool {
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: ragengineObj.Namespace,
+				Name:      ragengineObj.Name,
+			}, ragengineObj, &client.GetOptions{})
+
+			if err != nil {
+				return false
+			}
+
+			_, conditionFound := lo.Find(ragengineObj.Status.Conditions, func(condition metav1.Condition) bool {
+				return condition.Type == string(kaitov1alpha1.RAGEngineConditionTypeSucceeded) &&
+					condition.Status == metav1.ConditionTrue
+			})
+			return conditionFound
+		}, 10*time.Minute, utils.PollInterval).Should(BeTrue(), "Failed to wait for ragengine to be ready")
+	})
+}
+
+func cleanupResources(
+	workspaceObj *kaitov1alpha1.Workspace,
+	ragengineObj *kaitov1alpha1.RAGEngine,
+) {
+	By("Cleaning up resources", func() {
+		if !CurrentSpecReport().Failed() {
+			err := deleteRAGEngine(ragengineObj)
+			Expect(err).NotTo(HaveOccurred(), "Failed to delete RAGEngine")
+
+			err = deleteWorkspace(workspaceObj)
+			Expect(err).NotTo(HaveOccurred(), "Failed to delete Workspace")
+		} else {
+			if ragengineObj != nil {
+				GinkgoWriter.Printf("Test failed, keep Workspace %s and RAGEngine %s\n",
+					workspaceObj.Name, ragengineObj.Name)
+			} else {
+				GinkgoWriter.Printf("Test failed, keep Workspace %s\n", workspaceObj.Name)
+			}
+		}
+	})
+}
+
+func validateCreateNode(workspaceObj *kaitov1alpha1.Workspace, numOfNode int) {
+	utils.ValidateNodeClaimCreation(ctx, workspaceObj, numOfNode)
+}
+
+// validateWorkspacResourceStatus validates resource status
+func validateWorkspaceResourceStatus(workspaceObj *kaitov1alpha1.Workspace) {
+	By("Checking the resource status", func() {
+		Eventually(func() bool {
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: workspaceObj.Namespace,
+				Name:      workspaceObj.Name,
+			}, workspaceObj, &client.GetOptions{})
+
+			if err != nil {
+				return false
+			}
+
+			_, conditionFound := lo.Find(workspaceObj.Status.Conditions, func(condition metav1.Condition) bool {
+				return condition.Type == string(kaitov1alpha1.ConditionTypeResourceStatus) &&
+					condition.Status == metav1.ConditionTrue
+			})
+			return conditionFound
+		}, 10*time.Minute, utils.PollInterval).Should(BeTrue(), "Failed to wait for workspace resource status to be ready")
+	})
+}
+
+// validateRAGEngineResourceStatus validates resource status
+func validateRAGEngineResourceStatus(ragengineObj *kaitov1alpha1.RAGEngine) {
+	By("Checking the resource status", func() {
+		Eventually(func() bool {
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: ragengineObj.Namespace,
+				Name:      ragengineObj.Name,
+			}, ragengineObj, &client.GetOptions{})
+
+			if err != nil {
+				return false
+			}
+
+			_, conditionFound := lo.Find(ragengineObj.Status.Conditions, func(condition metav1.Condition) bool {
+				return condition.Type == string(kaitov1alpha1.ConditionTypeResourceStatus) &&
+					condition.Status == metav1.ConditionTrue
+			})
+			return conditionFound
+		}, 10*time.Minute, utils.PollInterval).Should(BeTrue(), "Failed to wait for ragengineObj resource status to be ready")
+	})
+}
+
+func validateAssociatedService(objectMeta metav1.ObjectMeta) {
+	serviceName := objectMeta.Name
+	serviceNamespace := objectMeta.Namespace
+
+	By(fmt.Sprintf("Checking for service %s in namespace %s", serviceName, serviceNamespace), func() {
+		service := &v1.Service{}
+
+		Eventually(func() bool {
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: serviceNamespace,
+				Name:      serviceName,
+			}, service)
+
+			if err != nil {
+				if errors.IsNotFound(err) {
+					GinkgoWriter.Printf("Service %s not found in namespace %s\n", serviceName, serviceNamespace)
+				} else {
+					GinkgoWriter.Printf("Error fetching service %s in namespace %s: %v\n", serviceName, serviceNamespace, err)
+				}
+				return false
+			}
+
+			GinkgoWriter.Printf("Found service: %s in namespace %s\n", serviceName, serviceNamespace)
+			return true
+		}, 10*time.Minute, utils.PollInterval).Should(BeTrue(), "Failed to wait for service to be created")
+	})
+}
+
+// validateInferenceConfig validates that the inference config exists and contains data
+func validateInferenceConfig(workspaceObj *kaitov1alpha1.Workspace) {
+	By("Checking the inference config exists", func() {
+		Eventually(func() bool {
+			configMap := &v1.ConfigMap{}
+			configName := kaitov1alpha1.DefaultInferenceConfigTemplate
+			if workspaceObj.Inference.Config != "" {
+				configName = workspaceObj.Inference.Config
+			}
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: workspaceObj.Namespace,
+				Name:      configName,
+			}, configMap)
+
+			if err != nil {
+				GinkgoWriter.Printf("Error fetching config: %v\n", err)
+				return false
+			}
+
+			return len(configMap.Data) > 0
+		}, 10*time.Minute, utils.PollInterval).Should(BeTrue(), "Failed to wait for inference config to be ready")
+	})
+}
+
+// validateInferenceResource validates inference deployment
+func validateInferenceandRAGResource(objectMeta metav1.ObjectMeta, expectedReplicas int32, isStatefulSet bool) {
+	By("Checking the inference resource", func() {
+		Eventually(func() bool {
+			var err error
+			var readyReplicas int32
+
+			if isStatefulSet {
+				sts := &appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      objectMeta.Name,
+						Namespace: objectMeta.Namespace,
+					},
+				}
+				err = utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+					Namespace: objectMeta.Namespace,
+					Name:      objectMeta.Name,
+				}, sts)
+				readyReplicas = sts.Status.ReadyReplicas
+
+			} else {
+				dep := &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      objectMeta.Name,
+						Namespace: objectMeta.Namespace,
+					},
+				}
+				err = utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+					Namespace: objectMeta.Namespace,
+					Name:      objectMeta.Name,
+				}, dep)
+				readyReplicas = dep.Status.ReadyReplicas
+			}
+
+			if err != nil {
+				GinkgoWriter.Printf("Error fetching resource: %v\n", err)
+				return false
+			}
+
+			if readyReplicas == expectedReplicas {
+				return true
+			}
+
+			return false
+		}, 20*time.Minute, utils.PollInterval).Should(BeTrue(), "Failed to wait for inference resource to be ready")
+	})
+}
+
+func deleteRAGEngine(ragengineObj *kaitov1alpha1.RAGEngine) error {
+	By("Deleting ragengineObj", func() {
+		Eventually(func() error {
+			// Check if the workspace exists
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: ragengineObj.Namespace,
+				Name:      ragengineObj.Name,
+			}, ragengineObj)
+
+			if errors.IsNotFound(err) {
+				GinkgoWriter.Printf("RAGEngine %s does not exist, no need to delete\n", ragengineObj.Name)
+				return nil
+			}
+			if err != nil {
+				return fmt.Errorf("error checking if ragengine %s exists: %v", ragengineObj.Name, err)
+			}
+
+			err = utils.TestingCluster.KubeClient.Delete(ctx, ragengineObj, &client.DeleteOptions{})
+			if err != nil {
+				return fmt.Errorf("failed to delete ragengine %s: %v", ragengineObj.Name, err)
+			}
+			return nil
+		}, utils.PollTimeout, utils.PollInterval).Should(Succeed(), "Failed to delete ragengine")
+	})
+
+	return nil
+}
+
+func deleteWorkspace(workspaceObj *kaitov1alpha1.Workspace) error {
+	By("Deleting workspace", func() {
+		Eventually(func() error {
+			// Check if the workspace exists
+			err := utils.TestingCluster.KubeClient.Get(ctx, client.ObjectKey{
+				Namespace: workspaceObj.Namespace,
+				Name:      workspaceObj.Name,
+			}, workspaceObj)
+
+			if errors.IsNotFound(err) {
+				GinkgoWriter.Printf("Workspace %s does not exist, no need to delete\n", workspaceObj.Name)
+				return nil
+			}
+			if err != nil {
+				return fmt.Errorf("error checking if workspace %s exists: %v", workspaceObj.Name, err)
+			}
+
+			err = utils.TestingCluster.KubeClient.Delete(ctx, workspaceObj, &client.DeleteOptions{})
+			if err != nil {
+				return fmt.Errorf("failed to delete workspace %s: %v", workspaceObj.Name, err)
+			}
+			return nil
+		}, utils.PollTimeout, utils.PollInterval).Should(Succeed(), "Failed to delete workspace")
+	})
+
+	return nil
+}

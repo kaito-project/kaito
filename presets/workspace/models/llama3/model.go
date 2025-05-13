a9cb6498f69f67caea3bfc9b999ee3e57e602e61
zhuangqh <zhuangqhc@gmail.com>
2025-05-13 18:02:48 +1000

    format
    Signed-off-by: zhuangqh <zhuangqhc@gmail.com>

--- presets/workspace/models/llama3/model.go
+++ presets/workspace/models/llama3/model.go
@@ -26,9 +26,9 @@ var (
 	baseCommandPresetLlamaInference = "accelerate launch"
 	// baseCommandPresetLlamaTuning    = "cd /workspace/tfs/ && python3 metrics_server.py & accelerate launch"
 	llamaRunParams = map[string]string{
-		"torch_dtype":   "bfloat16",
-		"pipeline":      "text-generation",
-		"chat_template": "/workspace/chat_templates/llama-3-instruct.jinja",
+		"torch_dtype":        "bfloat16",
+		"pipeline":           "text-generation",
+		"chat_template":      "/workspace/chat_templates/llama-3-instruct.jinja",
 		"allow_remote_files": "",
 	}
 	llamaRunParamsVLLM = map[string]string{

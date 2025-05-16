5245840f91da26fea2d62c76fa0a0829bf2218fe
Brandon Foley <brandonfoley13@gmail.com>
2025-05-16 17:11:06 -0400

    Move to using TransformComponent as base class

--- presets/ragengine/vector_store/transformers/custom_transformer.py
+++ presets/ragengine/vector_store/transformers/custom_transformer.py
@@ -2,18 +2,15 @@
 # Licensed under the MIT license.
 
 from llama_index.core.bridge.pydantic import PrivateAttr
-from llama_index.core.node_parser.interface import NodeParser, TextSplitter
 from llama_index.core.node_parser import SentenceSplitter, CodeSplitter
-from typing import Any, Dict, List, Sequence
-from llama_index.core.node_parser.node_utils import (
-    build_nodes_from_splits,
-)
+from typing import Dict, List
 from llama_index.core.schema import (
+    TransformComponent,
     BaseNode,
 )
 from llama_index.core.utils import get_tqdm_iterable
 
-class CustomTransformer(NodeParser):
+class CustomTransformer(TransformComponent):
     """Custom transformer for splitting documents based on metadata input."""
 
     _code_splitters: Dict[str, CodeSplitter] = PrivateAttr()
@@ -24,7 +21,7 @@ class CustomTransformer(NodeParser):
         self._code_splitters = {}
         self._sentence_splitter = SentenceSplitter()
 
-    def split_node(self, node: BaseNode) -> List[str]:
+    def split_node(self, node: BaseNode) -> List[BaseNode]:
         node_metadata = node.metadata
         split_type = node_metadata.get("split_type", "default")
         if split_type == "code":
@@ -36,20 +33,16 @@ class CustomTransformer(NodeParser):
                 self._code_splitters[langauge] = CodeSplitter(
                     language=langauge,
                 )
-            return self._code_splitters[langauge].split_text(node.text)
+            return self._code_splitters[langauge]([node])
         else:
             # Default to sentence splitting
-            return self._sentence_splitter.split_text(node.text)
-
-    def _parse_nodes(
-        self, nodes: Sequence[BaseNode], show_progress: bool = False, **kwargs: Any
-    ) -> List[BaseNode]:
+            return self._sentence_splitter([node])
+    
+    def __call__(self, nodes, **kwargs):
         all_nodes: List[BaseNode] = []
-        nodes_with_progress = get_tqdm_iterable(nodes, show_progress, "Parsing nodes")
-        for node in nodes_with_progress:
-            splits = self.split_node(node)
+        for node in nodes:
+            nodes = self.split_node(node)
             all_nodes.extend(
-                build_nodes_from_splits(splits, node, id_func=self.id_func)
+                self.split_node(node)
             )
-
         return all_nodes

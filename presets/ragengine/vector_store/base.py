8a8081b4ae9d2bd628bbf31bca80f3b79ae232ba
Brandon Foley <brandonfoley13@gmail.com>
2026-02-13 17:44:16 -0500

    remove unused import

--- presets/ragengine/vector_store/base.py
+++ presets/ragengine/vector_store/base.py
@@ -28,7 +28,6 @@ from llama_index.core import Document as LlamaDocument
 from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
 from llama_index.core.base.llms.types import MessageRole
 from llama_index.core.chat_engine.types import ChatMode
-from llama_index.core.retrievers import QueryFusionRetriever
 from llama_index.core.storage.docstore import SimpleDocumentStore
 from llama_index.core.vector_stores.types import (
     FilterCondition,

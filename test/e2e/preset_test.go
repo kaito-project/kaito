f43b4ee66bf45fe4f114891b0c6d2fb5649a9642
zhuangqh <zhuangqhc@gmail.com>
2026-02-17 17:15:29 +1100

    fix logs
    Signed-off-by: zhuangqh <zhuangqhc@gmail.com>

--- test/e2e/preset_test.go
+++ test/e2e/preset_test.go
@@ -1168,7 +1168,7 @@ var _ = Describe("Workspace Preset", func() {
 	AfterEach(func() {
 		if CurrentSpecReport().Failed() {
 			utils.PrintPodLogsOnFailure(namespaceName, "")     // The Preset Pod
-			utils.PrintPodLogsOnFailure("kaito-workspace", "") // The KAITO Workspace Pod
+			utils.PrintPodLogsOnFailure("kaito-workspace", "app.kubernetes.io/name=workspace") // The KAITO Workspace Pod
 			if !*skipGPUProvisionerCheck {
 				utils.PrintPodLogsOnFailure("gpu-provisioner", "") // The gpu-provisioner Pod
 			}

53923322e2d9478f767d864d63c68c82ea585b3a
mikemikimike <13286568797@163.com>
2026-09-09 14:22:46 +0800

    fix: align max model length status with v1beta1

--- pkg/workspace/controllers/model_metadata_test.go
+++ pkg/workspace/controllers/model_metadata_test.go
@@ -19,13 +19,13 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	kaitov1beta1 "github.com/kaito-project/kaito/api/v1beta1"
 	"github.com/kaito-project/kaito/pkg/featuregates"
 	"github.com/kaito-project/kaito/pkg/utils/consts"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type modelMetadataHTTPClientFunc func(*http.Request) (*http.Response, error)
